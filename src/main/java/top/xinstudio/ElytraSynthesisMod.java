f9d3c2b15c7116ce0010c689d29215cbf8a44d05
Star星辰 <1402832033@qq.com>
2024-02-23 00:08:54 +0800

    添加两种合成鞘翅的方法 ——第一种—— 腐肉 腐肉 腐肉 腐肉 腐肉 腐肉 腐肉 腐肉 腐肉 ——第二种—— 幻翼膜 幻翼膜 幻翼膜 幻翼膜  空   幻翼膜 幻翼膜  空   幻翼膜

--- src/main/java/top/xinstudio/ElytraSynthesisMod.java
+++ src/main/java/top/xinstudio/ElytraSynthesisMod.java
@@ -2,16 +2,8 @@ package top.xinstudio;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import top.xinstudio.registry.ModItemGroup;
-import top.xinstudio.registry.ModItems;
 
 public class ElytraSynthesisMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -20,17 +12,15 @@ public class ElytraSynthesisMod implements ModInitializer {
 	public static final String MOD_ID = "elytra-synthesis-mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger("elytra-synthesis-mod");
 
-//	private static final Item END_HEART = new Item(new Item.Settings().maxCount(32));
-
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-//		Registry.register(Registries.ITEM, new Identifier("es", "new_phantom_membrane"), END_HEART);
 
-		ModItems.registerModItems();
-		ModItemGroup.registerModItemGroup();
+//		ModItems.registerModItems();
+//		ModItemGroup.registerModItemGroup();
+
 		LOGGER.info("Hello Fabric world!");
 	}
 }
